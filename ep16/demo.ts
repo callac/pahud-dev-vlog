56119264a9d72a7f3c2a3bcb28e26de39bf4b252
Pahud Hsieh <pahudnet@gmail.com>
2020-05-05 12:06:10 +0800

    Update demo.ts

--- ep16/demo.ts
+++ ep16/demo.ts
@@ -3,13 +3,13 @@ import * as cdk from '@aws-cdk/core';
 import * as apigatewayv2 from '../lib';
 
 const app = new cdk.App();
+const stack = new cdk.Stack(app, 'APIDemo');
 
-const env = {
-  region: process.env.CDK_DEFAULT_REGION,
-  account: process.env.CDK_DEFAULT_ACCOUNT
-};
-
-const stack = new cdk.Stack(app, 'PahudDevDemo', { env });
+const api = new apigatewayv2.HttpApi(stack, 'Api', {
+  defaultIntegration: new apigatewayv2.HttpProxyIntegration({
+    url: 'https://pahud.dev',
+  })
+});
 
 const echoHandler = new lambda.Function(stack, 'echoHandler', {
   runtime: lambda.Runtime.NODEJS_12_X,
@@ -17,52 +17,54 @@ const echoHandler = new lambda.Function(stack, 'echoHandler', {
   code: new lambda.InlineCode('exports.handler = async function(event, context) { return { statusCode: 200, body: JSON.stringify(event) }; };'),
 });
 
-const listHandler = new lambda.Function(stack, 'listHandler', {
+const listBooksHandler = new lambda.Function(stack, 'listBooksHandler', {
   runtime: lambda.Runtime.NODEJS_12_X,
   handler: 'index.handler',
   code: new lambda.InlineCode('exports.handler = async function(event, context) { return { statusCode: 200, body: "list books" }; };'),
 });
 
-const getBookHandler = new lambda.Function(stack, 'getBookHandler', {
+const bookHandler = new lambda.Function(stack, 'bookHandler', {
   runtime: lambda.Runtime.NODEJS_12_X,
   handler: 'index.handler',
-  code: new lambda.InlineCode('exports.handler = async function(event, context) { return { statusCode: 200, body: "here you are: book_id=" + event.pathParameters.book_id }; };'),
+  code: new lambda.InlineCode('exports.handler = async function(event, context) { return { statusCode: 200, body: "Here you are: book_id=" + event.pathParameters.book_id }; };'),
 });
 
-const api = new apigatewayv2.HttpApi(stack, 'HttpApi', {
-  defaultIntegration: new apigatewayv2.HttpProxyIntegration({
-    url: 'https://pahud.dev'
-  })
-});
-
-// ANY /echo
+// HTTP ANY /echo
 api.addRoutes({
-  integration: new apigatewayv2.LambdaProxyIntegration({ handler: echoHandler }),
-  path: '/echo',
+  integration: new apigatewayv2.LambdaProxyIntegration({
+    handler: echoHandler
+  }),
+  path: '/echonow'
 });
 
-// GET /books
+// HTTP GET|POST /books
 api.addRoutes({
-  integration: new apigatewayv2.LambdaProxyIntegration({ handler: listHandler }),
+  integration: new apigatewayv2.LambdaProxyIntegration({
+    handler: listBooksHandler
+  }),
   path: '/books',
-  methods: [ apigatewayv2.HttpMethod.GET ]
+  methods: [ apigatewayv2.HttpMethod.GET, apigatewayv2.HttpMethod.POST ]
 });
 
-// GET /books/{book_id}
+// HTTP GET DELETE POST /books/{book_id}
 api.addRoutes({
-  integration: new apigatewayv2.LambdaProxyIntegration({ handler: getBookHandler }),
+  integration: new apigatewayv2.LambdaProxyIntegration({
+    handler: bookHandler
+  }),
   path: '/books/{book_id}',
-  methods: [apigatewayv2.HttpMethod.GET]
+  methods: [apigatewayv2.HttpMethod.GET, apigatewayv2.HttpMethod.POST]
 });
 
-new cdk.CfnOutput(stack, 'ApiUrl', {
-  value: `https://${api.httpApiId}.execute-api.${cdk.Stack.of(stack).region}.${cdk.Stack.of(stack).urlSuffix}`
+api.addStage('BetaStage', {
+  stageName: 'beta',
+  autoDeploy: true
 });
 
-new cdk.CfnOutput(stack, 'listBooks', {
-  value: `https://${api.httpApiId}.execute-api.${cdk.Stack.of(stack).region}.${cdk.Stack.of(stack).urlSuffix}/books`
-});
+const endpointUrl = `https://${api.httpApiId}.execute-api.${cdk.Stack.of(stack).region}.${cdk.Stack.of(stack).urlSuffix
+}`;
 
-new cdk.CfnOutput(stack, 'getBooks', {
-  value: `https://${api.httpApiId}.execute-api.${cdk.Stack.of(stack).region}.${cdk.Stack.of(stack).urlSuffix}/books/{book_id}`
-});
+new cdk.CfnOutput(stack, 'ApiURL', { value: endpointUrl });
+new cdk.CfnOutput(stack, 'BetaApiURL', { value: `${endpointUrl}/beta` });
+new cdk.CfnOutput(stack, 'EchoURL', { value: `${endpointUrl}/echo` });
+new cdk.CfnOutput(stack, 'ListBooksURL', { value: `${endpointUrl}/books`});
+new cdk.CfnOutput(stack, 'BookURL', { value: `${endpointUrl}/books/{book_id}`});
